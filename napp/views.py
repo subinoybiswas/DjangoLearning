f9ab408532459f7929efb2f29fc96a5b0599142f
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-02-14 00:23:59 +0530

    Http404 instead of 500

--- napp/views.py
+++ napp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Notes
+from django.http import Http404
 # Create your views here.
 def list(request):
     all_notes =Notes.objects.all()
     return render(request,'napp/listp.html',{'notes':all_notes})
 def detail(request,tk):
-    notee = Notes.objects.get(pk=tk)
+    try:
+        notee = Notes.objects.get(pk=tk)
+    except Notes.DoesNotExist:
+        raise Http404
     return render(request,'napp/detail.html',{'note':notee})
