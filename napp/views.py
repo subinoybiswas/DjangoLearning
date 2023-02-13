a937e436c503299794879cc260cd08fbf80779ec
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-02-14 00:19:18 +0530

    Individual Pages for notes

--- napp/views.py
+++ napp/views.py
@@ -4,3 +4,6 @@ from .models import Notes
 def list(request):
     all_notes =Notes.objects.all()
     return render(request,'napp/listp.html',{'notes':all_notes})
+def detail(request,tk):
+    notee = Notes.objects.get(pk=tk)
+    return render(request,'napp/detail.html',{'note':notee})
